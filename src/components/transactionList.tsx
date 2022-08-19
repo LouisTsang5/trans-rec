6b9b214b728d29caf4e0f8b074edb42d6b02a3ce
louistsang5 <louistsang5@gmail.com>
2022-08-19 21:05:49 +0800

    updated transaction list style

--- src/components/transactionList.tsx
+++ src/components/transactionList.tsx
@@ -7,23 +7,47 @@ type TransactionList = {
 
 const TransactionList: FunctionComponent<TransactionList> = ({ list, onRemoveTransaction }) => {
     return (
-        <table>
+        <table className='table table-hover max-vw-100' style={{ tableLayout: 'fixed' }}>
             <thead>
                 <tr>
-                    <th>Date</th>
-                    <th>Type</th>
-                    <th>Desc</th>
-                    <th>Amt</th>
+                    <th style={{ width: '20%' }}>Date</th>
+                    <th style={{ width: '45%', textAlign: 'center' }}>Description</th>
+                    <th style={{ width: '25%', textAlign: 'right' }}>Amt</th>
+                    <th style={{ width: '10%' }}></th>
                 </tr>
             </thead>
             <tbody>
                 {list.map(data => (
                     <tr key={data.id}>
-                        <td>{data.date.toLocaleDateString('en-GB', { day: '2-digit', month: 'short' })}</td>
-                        <td>{data.type.toUpperCase()}</td>
-                        <td>{data.description}</td>
-                        <td>{data.amount}</td>
-                        <td><button onClick={() => onRemoveTransaction(data.id)}>x</button></td>
+                        <td>
+                            {data.date.toLocaleDateString('en-GB', { day: '2-digit', month: 'short' })}
+                        </td>
+
+                        <td
+                            style={{ overflow: 'hidden', textOverflow: 'ellipsis', whiteSpace: 'nowrap' }}
+                        >
+                            {data.description}
+                        </td>
+
+                        <td
+                            style={{
+                                overflow: 'hidden',
+                                textOverflow: 'ellipsis',
+                                whiteSpace: 'nowrap',
+                                textAlign: 'right',
+                                color: data.type === 'd' ? 'green' : 'red'
+                            }}
+                        >
+                            {`${data.type === 'd' ? '+' : '-'}${data.amount.toFixed(2)}`}
+                        </td>
+
+                        <td onClick={() => onRemoveTransaction(data.id)}>
+                            <div className='d-flex justify-content-center align-items-end'>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
+                                    <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                                </svg>
+                            </div>
+                        </td>
                     </tr>
                 ))}
             </tbody>

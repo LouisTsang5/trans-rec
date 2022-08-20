bf2d2248609e285b6cbc482d72a1d3640d2e8e5a
louistsang5 <louistsang5@gmail.com>
2022-08-20 21:25:18 +0800

    added routing to home page for logo

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -1,9 +1,11 @@
 import { FunctionComponent } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const NavBar: FunctionComponent = () => {
+    const navigate = useNavigate();
     return (
         <nav className="navbar navbar-light bg-light px-1 d-flex justify-content-center">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand" href="#" onClick={() => navigate('/')}>
                 <img src="/favicon.ico" width={30} height={30} className="d-inline-block align-top" />
                 <span className="px-1">TransRec</span>
             </a>

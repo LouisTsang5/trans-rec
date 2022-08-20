312c20c65f11794245a4ab1f25efd4ae755a0f80
louistsang5 <louistsang5@gmail.com>
2022-08-20 17:40:42 +0800

    moved main components to home page

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,13 @@
-import { FunctionComponent, useEffect, useMemo, useState } from 'react';
-import TransactionInput from './components/transactionInput/transactionInput';
-import TransactionList from './components/transactionList/transactionList';
+import { FunctionComponent, useEffect, useState } from 'react';
 import { loadList, storeList } from './lib/localStorage';
-import { calculateBalance } from './lib/transaction';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBar from './components/navBar';
-import Balance from './components/balance';
+import Home from './pages/home';
 
 const startingList = loadList() ?? [];
 
 const App: FunctionComponent = () => {
     const [transactionList, setTransactionList] = useState(startingList);
-    const balance = useMemo(() => calculateBalance(transactionList), [transactionList]);
 
     //Store to local storage
     useEffect(() => {
@@ -23,18 +19,19 @@ const App: FunctionComponent = () => {
         setTransactionList(newList);
     };
 
+    const addTransaction = (data: TransactionData) => {
+        setTransactionList(prev => [...prev, data]);
+    };
+
     return (
         <div>
             <NavBar />
             <div className='px-2'>
-                <TransactionInput
-                    onAddTransaction={(data) => setTransactionList(prev => [...prev, data])}
-                />
-                <TransactionList
-                    list={transactionList}
+                <Home
+                    transactionList={transactionList}
+                    onAddTransaction={addTransaction}
                     onRemoveTransaction={removeTransaction}
                 />
-                <Balance balance={balance} />
             </div>
         </div>
     );
---
+++ src/pages/home.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent, useMemo } from 'react';
+import Balance from '../components/balance';
+import TransactionInput from '../components/transactionInput/transactionInput';
+import TransactionList from '../components/transactionList/transactionList';
+import { calculateBalance } from '../lib/transaction';
+
+type HomeProps = {
+    transactionList: TransactionData[],
+    onAddTransaction: (data: TransactionData) => void,
+    onRemoveTransaction: (id: string) => void,
+}
+
+const Home: FunctionComponent<HomeProps> = ({ transactionList, onAddTransaction, onRemoveTransaction }) => {
+    const balance = useMemo(() => calculateBalance(transactionList), [transactionList]);
+    return (
+        <>
+            <TransactionInput
+                onAddTransaction={onAddTransaction}
+            />
+            <TransactionList
+                list={transactionList}
+                onRemoveTransaction={onRemoveTransaction}
+            />
+            <Balance balance={balance} />
+        </>
+    );
+};
+
+export default Home;

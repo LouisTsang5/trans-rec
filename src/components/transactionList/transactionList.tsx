94c8cba55c307f1f077871187198ec9aa67947fb
louistsang5 <louistsang5@gmail.com>
2022-08-20 21:54:31 +0800

    fixed issue of after delete is click, the app tries to navigate to the deleted transaction

--- src/components/transactionList/transactionList.tsx
+++ src/components/transactionList/transactionList.tsx
@@ -9,6 +9,11 @@ type TransactionList = {
 
 const TransactionList: FunctionComponent<TransactionList> = ({ list, onRemoveTransaction }) => {
     const navigate = useNavigate();
+
+    const onClickTransaction = (id: string) => {
+        navigate(`/transactions/${id}`);
+    };
+
     return (
         <>
             <table className='table mb-0'>
@@ -25,12 +30,12 @@ const TransactionList: FunctionComponent<TransactionList> = ({ list, onRemoveTra
                 <table className='table'>
                     <tbody>
                         {list.map(data => (
-                            <tr key={data.id} onClick={() => navigate(`/transactions/${data.id}`)}>
-                                <td className='col-date'>
+                            <tr key={data.id}>
+                                <td className='col-date' onClick={onClickTransaction.bind(undefined, data.id)}>
                                     {data.date.toLocaleDateString('en-GB', { day: '2-digit', month: 'short' })}
                                 </td>
 
-                                <td className='col desc'>
+                                <td className='col desc' onClick={onClickTransaction.bind(undefined, data.id)}>
                                     {data.description}
                                 </td>
 
@@ -41,7 +46,7 @@ const TransactionList: FunctionComponent<TransactionList> = ({ list, onRemoveTra
                                     {`${data.type === 'd' ? '+' : '-'}${data.amount.toFixed(2)}`}
                                 </td>
 
-                                <td className='col-del' onClick={() => onRemoveTransaction(data.id)}>
+                                <td className='col-del' onClick={onRemoveTransaction.bind(undefined, data.id)}>
                                     <div className='d-flex justify-content-center align-items-end'>
                                         <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" fill="currentColor" className="bi bi-x" viewBox="0 0 16 16">
                                             <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />

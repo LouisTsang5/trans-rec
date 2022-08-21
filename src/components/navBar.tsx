87b083dda2a4ad7d6c45174c40e1e73d93dbf289
louistsang5 <louistsang5@gmail.com>
2022-08-21 17:14:07 +0800

    added nav menu to nav bar

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -4,12 +4,27 @@ import { useNavigate } from 'react-router-dom';
 const NavBar: FunctionComponent = () => {
     const navigate = useNavigate();
     return (
-        <nav className="navbar navbar-light bg-light px-1 d-flex justify-content-center">
-            <a className="navbar-brand" href="#" onClick={() => navigate('/')}>
-                <img src="/favicon.ico" width={30} height={30} className="d-inline-block align-top" />
-                <span className="px-1">TransRec</span>
-            </a>
-        </nav>
+        <nav className="navbar navbar-light bg-light px-1 d-flex">
+            <div className='w-25' style={{ visibility: 'hidden' }}>A</div>
+
+            <div className='w-50 d-flex justify-content-center'>
+                <a className="navbar-brand" href="#" onClick={() => navigate('/')} style={{ marginRight: 0 }}>
+                    <img src="/favicon.ico" width={30} height={30} className="d-inline-block align-top" />
+                    <span className="px-1">TransRec</span>
+                </a>
+            </div>
+
+            <div className='w-25 d-flex justify-content-end'>
+                <div className="dropdown">
+                    <button className="btn-sm navbar-toggler" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false" style={{ border: 'none' }}>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+                        <li><a className="dropdown-item" href="#" onClick={() => navigate('save')}>Save</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav >
     );
 };
 

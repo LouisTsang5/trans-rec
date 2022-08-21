15faa7d4aa85318e9b4429ece2e9742d820a6b5c
louistsang5 <louistsang5@gmail.com>
2022-08-21 12:58:28 +0800

    added function to parse string to transaction list for save loading

--- src/lib/localStorage.ts
+++ src/lib/localStorage.ts
@@ -1,3 +1,6 @@
+import { transactionTypeDisplayMap } from './transaction';
+import { hasKey, isValidDate } from './util';
+
 type ParsedTransactionData = {
     amount: number,
     date: string,
@@ -8,6 +11,59 @@ type ParsedTransactionData = {
 
 const key = 'transactionList';
 
+function toTransactionData(parsedData: ParsedTransactionData): TransactionData {
+    return {
+        amount: parsedData.amount,
+        date: new Date(parsedData.date),
+        description: parsedData.description,
+        id: parsedData.id,
+        type: parsedData.type,
+    };
+}
+
+function isValidTransaction(data: unknown): data is ParsedTransactionData {
+    if (!data || typeof data !== 'object') return false;
+
+    //Check date
+    const dateKey = 'date';
+    if (
+        !hasKey(data, dateKey) ||
+        typeof data[dateKey] !== 'string' ||
+        !isValidDate(new Date(data[dateKey]))
+    ) return false;
+
+    //Check amount
+    const amtKey = 'amount';
+    if (
+        !hasKey(data, amtKey) ||
+        typeof data[amtKey] !== 'number'
+    ) return false;
+
+    //Check description
+    const descKey = 'description';
+    if (
+        !hasKey(data, descKey) ||
+        typeof data[descKey] !== 'string'
+    ) return false;
+
+    //Check id
+    const idKey = 'id';
+    if (
+        !hasKey(data, idKey) ||
+        typeof data[idKey] !== 'string'
+    ) return false;
+
+    //Check type
+    const typeKey = 'type';
+    if (
+        !hasKey(data, typeKey) ||
+        typeof data[typeKey] !== 'string' ||
+        !Object.keys(transactionTypeDisplayMap).includes(data[typeKey])
+    ) return false;
+
+    return true;
+}
+
 export function storeList(list: TransactionData[]) {
     localStorage.setItem(key, JSON.stringify(list));
 }
@@ -15,11 +71,15 @@ export function storeList(list: TransactionData[]) {
 export function loadList(): TransactionData[] | undefined {
     const str = localStorage.getItem(key);
     const parsedData = str ? JSON.parse(str) as ParsedTransactionData[] : undefined;
-    return parsedData?.map(pData => ({
-        amount: pData.amount,
-        date: new Date(pData.date),
-        description: pData.description,
-        id: pData.id,
-        type: pData.type,
-    } as TransactionData));
+    return parsedData?.map(pData => toTransactionData(pData));
+}
+
+export function parseData(dataStr: string): TransactionData[] {
+    const data = JSON.parse(dataStr);
+    if (!Array.isArray(data)) throw new Error('Incorrect save format');
+    const list = data.map(item => {
+        if (!isValidTransaction(item)) throw new Error('Incorrect save format');
+        return toTransactionData(item);
+    });
+    return list;
 }

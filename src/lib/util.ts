1133e7898d2676e8d8311b4bb7b9a9c415a74a7e
louistsang5 <louistsang5@gmail.com>
2022-11-27 20:42:45 +0800

    add function to move elements in an array immutably

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -5,3 +5,18 @@ export function hasKey<T>(object: T, key: PropertyKey): key is keyof object {
 export function isValidDate(date: Date) {
     return date instanceof Date && !isNaN(date.getTime());
 }
+
+export function immutableMove<T>(arr: T[], fromIndex: number, toIndex: number) {
+    const newArr: T[] = [];
+    const targetItem = arr[fromIndex];
+
+    if (toIndex === -1) newArr.push(targetItem);
+    arr.forEach((item, index) => {
+        newArr.push(item);
+        if (index === toIndex) newArr.push(targetItem);
+    });
+
+    const removeIndex = toIndex < fromIndex ? fromIndex + 1 : fromIndex;
+    newArr.splice(removeIndex, 1);
+    return newArr;
+}

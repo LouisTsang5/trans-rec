963bbe221c177f184d0fbebadffbb710b2f00357
louistsang <louis.tsang@ricoh.com.hk>
2023-03-30 17:17:18 +0800

    update bus search page to be able to search bus routes and display a list of buses

--- src/pages/busSearch.tsx
+++ src/pages/busSearch.tsx
@@ -1,20 +1,57 @@
-import { FunctionComponent, useState } from 'react';
+import { FunctionComponent, useMemo, useState } from 'react';
+import { getRoutes } from '../lib/busStop/kmb';
+import { debounce } from '../lib/util';
 
-export const BusSearch: FunctionComponent = () => {
-    const [busNum, setBusNum] = useState('');
+type BusInfo = {
+    number: string,
+    from: string,
+    to: string,
+}
 
+const MAX_LIST_ITEMS = 50;
+const DEBOUNCE_DELAY_MS = 500;
+
+async function searchBus(busNum: string) {
+    const allBuses = await getRoutes();
+    const busInfoList: BusInfo[] = allBuses.map(b => ({ number: b.route, from: b.orig_tc, to: b.dest_tc }));
+    return busInfoList.filter(b => b.number.startsWith(busNum)).slice(0, MAX_LIST_ITEMS).sort((a, b) => a.number.localeCompare(b.number));
+}
+
+const SearchBar: FunctionComponent<{ onSearch: (busNum: string) => void }> = ({ onSearch }) => {
+    const debouncedSearch = useMemo(() => debounce(onSearch, DEBOUNCE_DELAY_MS), []);
     return (
         <form>
             <div className='col-12 form-group'>
                 <span>Bus</span>
                 <input
-                    onFocus={() => setBusNum('')}
-                    onChange={e => setBusNum(e.target.value.toUpperCase())}
-                    value={busNum}
+                    onFocus={e => e.target.value = ''}
+                    onChange={e => debouncedSearch(e.target.value.toUpperCase())}
                     className={`form-control`}
                     type="text"
                 />
             </div>
         </form>
+    );
+};
+
+const BusList: FunctionComponent<{ list: BusInfo[] }> = ({ list }) => {
+    return (
+        <ul>
+            {list.map(({ number, from, to }, i) => (
+                <li key={i}><span>{number} {from} {'->'} {to}</span></li>
+            ))}
+        </ul>
+    );
+};
+
+export const BusSearch: FunctionComponent = () => {
+    const [busInfoList, setBusInfoList] = useState<BusInfo[]>([]);
+    const search = async (num: string) => setBusInfoList(await searchBus(num));
+
+    return (
+        <>
+            <SearchBar onSearch={search} />
+            <BusList list={busInfoList} />
+        </>
     );
 };

ddd67e4a708a94965918f17c4889c6aafaf79833
louistsang <louis.tsang@ricoh.com.hk>
2023-03-31 16:50:19 +0800

    cache bus route lists instead of request a new list on every search

--- src/pages/busSearch.tsx
+++ src/pages/busSearch.tsx
@@ -1,18 +1,12 @@
-import { FunctionComponent, useMemo, useState } from 'react';
+import { FunctionComponent, useEffect, useMemo, useState } from 'react';
 import { getAllRoutes, RouteInfo } from '../lib/busStop/common';
 import { debounce } from '../lib/util';
 
 const MAX_LIST_ITEMS = 50;
 const DEBOUNCE_DELAY_MS = 500;
 
-async function searchBus(busNum: string) {
-    if (!busNum) return [];
-    const allRoutes = await getAllRoutes();
-    return allRoutes.filter(b => b.number.startsWith(busNum)).slice(0, MAX_LIST_ITEMS).sort((a, b) => a.number.localeCompare(b.number));
-}
-
 const SearchBar: FunctionComponent<{ onSearch: (busNum: string) => void }> = ({ onSearch }) => {
-    const debouncedSearch = useMemo(() => debounce(onSearch, DEBOUNCE_DELAY_MS), []);
+    const debouncedSearch = useMemo(() => debounce(onSearch, DEBOUNCE_DELAY_MS), [onSearch]);
     return (
         <form>
             <div className='col-12 form-group'>
@@ -43,13 +37,34 @@ const BusList: FunctionComponent<{ list: RouteInfo[] }> = ({ list }) => {
 };
 
 export const BusSearch: FunctionComponent = () => {
-    const [busInfoList, setBusInfoList] = useState<RouteInfo[]>([]);
-    const search = async (num: string) => setBusInfoList(await searchBus(num));
+    const [routesList, setRoutesList] = useState<RouteInfo[]>([]);
+    const [isLoadingList, setIsLoadingList] = useState(false);
+    const refreshRoutes = async () => {
+        setIsLoadingList(true);
+        setRoutesList(await getAllRoutes());
+        setIsLoadingList(false);
+    };
+    useEffect(() => {
+        refreshRoutes();
+    }, []);
+
+    const [hitList, setHitList] = useState<RouteInfo[]>([]);
+    const search = useMemo(() => (route: string) => {
+        if (!route) return setHitList([]);
+        setHitList(routesList.filter(b => b.number.startsWith(route)).slice(0, MAX_LIST_ITEMS).sort((a, b) => a.number.localeCompare(b.number)));
+    }, [routesList]);
 
     return (
         <>
-            <SearchBar onSearch={search} />
-            <BusList list={busInfoList} />
+            {
+                isLoadingList ?
+                    <span>Loading bus list...</span> :
+                    <>
+                        <SearchBar onSearch={search} />
+                        <BusList list={hitList} />
+                    </>
+
+            }
         </>
     );
 };

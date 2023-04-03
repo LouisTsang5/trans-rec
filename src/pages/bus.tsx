d470251864e38fde791389a17a244afb38ed1d37
louistsang <louis.tsang@ricoh.com.hk>
2023-04-03 15:29:17 +0800

    draft stop eta page

--- src/pages/bus.tsx
+++ src/pages/bus.tsx
@@ -1,6 +1,8 @@
 import { FunctionComponent, useEffect, useMemo, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { getAllRoutes, RouteInfo } from '../lib/busStop/common';
+import { Route, Routes, useNavigate, useSearchParams } from 'react-router-dom';
+import { Bound, getAllRoutes, isCtbRoute, isKmbRoute, RouteInfo } from '../lib/busStop/common';
+import { getStopsEtas as getKmbStopsEtas } from '../lib/busStop/kmb';
+import { getStopsEtas as getCtbStopsEtas, Company as CtbCompany } from '../lib/busStop/ctb';
 import { debounce } from '../lib/util';
 
 const MAX_LIST_ITEMS = 50;
@@ -27,18 +29,35 @@ const SearchBar: FunctionComponent<{ onSearch: (busNum: string) => void }> = ({ 
     );
 };
 
+function getQueryParam({ data, bound }: RouteInfo): URLSearchParams {
+    if (isCtbRoute(data)) {
+        const { route, co: company } = data;
+        return new URLSearchParams({ route, bound, company });
+    } else if (isKmbRoute(data)) {
+        const { route, service_type: servicetype } = data;
+        return new URLSearchParams({ route, bound, company: 'KMB', servicetype });
+    } else {
+        const { route } = data;
+        return new URLSearchParams({ route, bound, company: 'MTR' });
+    }
+}
+
 const BusList: FunctionComponent<{ list: RouteInfo[] }> = ({ list }) => {
+    const navigate = useNavigate();
     return (
         <ul className='list-group'>
-            {list.map(({ number, from, to, company }, i) => (
-                <li key={i} className='list-group-item' style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gridTemplateRows: 'repeat(3, 1fr)' }} >
-                    <div style={{ gridRow: 'span 3', gridColumn: '1', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                        <span style={{ fontSize: '1.8rem', fontWeight: 'bold', textAlign: 'center' }}>{number}</span>
-                    </div>
-                    <span style={{ gridRow: '1 / span 2', gridColumn: '2 / span 4', fontSize: '1.2rem', paddingLeft: '1rem' }}>{`${from} -> ${to}`}</span>
-                    <span style={{ gridRow: '3', gridColumn: '2 / span 4', fontSize: '0.8rem', paddingLeft: '1rem' }}>{company}</span>
-                </li>
-            ))}
+            {list.map((route, i) => {
+                const { number, from, to, company } = route;
+                return (
+                    <li key={i} className='list-group-item' style={{ display: 'grid', gridTemplateColumns: 'repeat(5, 1fr)', gridTemplateRows: 'repeat(3, 1fr)' }} onClick={() => navigate({ pathname: 'schedule', search: `?${getQueryParam(route)}` })}>
+                        <div style={{ gridRow: 'span 3', gridColumn: '1', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                            <span style={{ fontSize: '1.8rem', fontWeight: 'bold', textAlign: 'right' }}>{number}</span>
+                        </div>
+                        <span style={{ gridRow: '1 / span 2', gridColumn: '2 / span 4', fontSize: '1.2rem', paddingLeft: '1rem' }}>{`${from} -> ${to}`}</span>
+                        <span style={{ gridRow: '3', gridColumn: '2 / span 4', fontSize: '0.8rem', paddingLeft: '1rem' }}>{company}</span>
+                    </li>
+                );
+            })}
         </ul>
     );
 };
@@ -79,6 +98,20 @@ const BusSearch: FunctionComponent = () => {
 };
 
 const BusSchedule: FunctionComponent = () => {
+    const [searchParams] = useSearchParams();
+    const route = searchParams.get('route');
+    const bound = searchParams.get('bound') as Bound;
+    const company = searchParams.get('company');
+    const serviceType = searchParams.get('servicetype');
+
+    // If not enough search params return error
+    const errorJsx = <span>Not enough arguments</span>;
+    if (!route || !bound || !company) return errorJsx;
+    if (company === 'KMB' && !serviceType) return errorJsx;
+
+    const pStopsEtas = company === 'KMB' ? getKmbStopsEtas(route, bound, serviceType as string) : getCtbStopsEtas(company as CtbCompany, route, bound);
+    pStopsEtas.then(se => console.log(se));
+
     return <span>Schedule</span>;
 };
 
